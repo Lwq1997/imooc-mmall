2950ad1f16da0fdd3be9a77c0c971a583e068c85
Lwq <943904454@qq.com>
2019-03-24 14:30:44 +0800

    project init commit

---
+++ src/main/java/com/lwq/mmall/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.lwq.mmall.dao;
+
+import com.lwq.mmall.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
---
+++ src/main/java/com/lwq/mmall/dao/ShippingMapper.java
@@ -0,0 +1,17 @@
+package com.lwq.mmall.dao;
+
+import com.lwq.mmall.pojo.Shipping;
+
+public interface ShippingMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Shipping record);
+
+    int insertSelective(Shipping record);
+
+    Shipping selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Shipping record);
+
+    int updateByPrimaryKey(Shipping record);
+}
